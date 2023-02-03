d5bece76de31426da871b3aff61b7cb493c63ed7
mahimagupta24 <mahimagupta2401@gmail.com>
2023-02-03 16:40:53 +0530

    refactor

--- src/Button.js
+++ src/Button.js
@@ -3,12 +3,11 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button
-      className={classes.button}>
-       {/* type={props.type || "button"}> */}
-      {/* onClick={props.onClick}> */}
+    <button className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}>
       {props.children}
-    </button>
+    </button>//type and onclick props not understood
   );
 };
 export default Button;
